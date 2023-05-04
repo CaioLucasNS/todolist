d9f1b3426c8690cd6b518fb7fe69fcb98296563a
Caio Lucas <lcaio1281@gmail.com>
2023-05-04 00:52:53 -0300

    header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../theme/theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    height: 173,
+    width: "100%",
+    backgroundColor: theme.gray700,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,19 @@
 import { Text, SafeAreaView } from "react-native";
+import { Header } from "../../components/Header";
 import { styles } from "./styles";
 
 export function Home() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.notTaskTitle}>
-        Você ainda não tem tarefas cadastradas
-      </Text>
-      <Text style={styles.notTaskDescription}>
-        Crie tarefas e organize seus itens a fazer
-      </Text>
-    </SafeAreaView>
+    <>
+      <Header />
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.notTaskTitle}>
+          Você ainda não tem tarefas cadastradas
+        </Text>
+        <Text style={styles.notTaskDescription}>
+          Crie tarefas e organize seus itens a fazer
+        </Text>
+      </SafeAreaView>
+    </>
   );
 }

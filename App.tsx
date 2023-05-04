fee2d3074c634fc598e296eccd686c960449cccc
Caio Lucas <lcaio1281@gmail.com>
2023-05-04 00:13:06 -0300

    setting up global theme

--- App.tsx
+++ App.tsx
@@ -2,7 +2,13 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <Text>To Do List</Text>
     </View>
   );
